5184611abe015662057617dd9cb003499c08aca8
monosans <hsyqixco@protonmail.com>
2021-11-13 10:06:20 +0300

    Fix month test

--- tests/test_month.py
+++ tests/test_month.py
@@ -10,4 +10,4 @@ async def test_month() -> None:
     gm = await gismeteo("weather-moscow-4368")
     month = await gm.month()
     for attr in (month.status, month.max_temperature, month.min_temperature):
-        check_dict(attr, 27, 31)
+        check_dict(attr, 21, 31)
