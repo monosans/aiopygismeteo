a1570fc89df0b5efef340fea5454a00c2b36df56
monosans <hsyqixco@protonmail.com>
2021-12-06 21:32:17 +0300

    Fix month tests

--- tests/utils.py
+++ tests/utils.py
@@ -6,7 +6,7 @@ def check_dict(
     d: Dict[str, str], length: int, max_length: Optional[int] = None
 ) -> None:
     if max_length:
-        assert length <= len(d) < max_length
+        assert length <= len(d) <= max_length
     else:
         assert len(d) == length
     for key, value in d.items():
