f4f1cea49cd5effc497b23ceb24ff3cc1e78fb97
monosans <hsyqixco@protonmail.com>
2023-04-22 21:32:54 +0300

    Add types module

--- aiopygismeteo/__init__.py
+++ aiopygismeteo/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from pygismeteo_base import models
 
+from . import types
 from ._gismeteo import Gismeteo
 
-__all__ = ("Gismeteo", "models")
+__all__ = ("Gismeteo", "models", "types")
---
+++ aiopygismeteo/types.py
@@ -0,0 +1,6 @@
+"""Может пригодиться для типизации кода, использующего библиотеку."""
+from __future__ import annotations
+
+from pygismeteo_base.types import Lang, SearchLimit, Step3Days, Step6Days, Step24Days
+
+__all__ = ("Lang", "SearchLimit", "Step3Days", "Step6Days", "Step24Days")
