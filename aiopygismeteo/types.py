eb3a3c87e20b5b3ccdde5e48247dd2fe3d596785
monosans <hsyqixco@protonmail.com>
2023-05-01 12:09:46 +0300

    Add more types to types.py

--- aiopygismeteo/types.py
+++ aiopygismeteo/types.py
@@ -3,4 +3,19 @@ from __future__ import annotations
 
 from pygismeteo_base.types import Lang, SearchLimit, Step3Days, Step6Days, Step24Days
 
-__all__ = ("Lang", "SearchLimit", "Step3Days", "Step6Days", "Step24Days")
+from ._current import Current
+from ._search import Search
+from ._step_n import Step3, Step6, Step24
+
+__all__ = (
+    "Current",
+    "Lang",
+    "Search",
+    "SearchLimit",
+    "Step24",
+    "Step24Days",
+    "Step3",
+    "Step3Days",
+    "Step6",
+    "Step6Days",
+)
