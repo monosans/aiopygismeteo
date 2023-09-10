425d6b851d635a99ca58515542557e082181d7c9
monosans <hsyqixco@protonmail.com>
2023-09-10 19:22:24 +0300

    Improve tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,62 @@
 from __future__ import annotations
 
-import pytest
+import asyncio
+from typing import AsyncIterator, Iterator, Tuple
 
-try:
-    from pydantic.v1 import BaseConfig
-except ImportError:  # pragma: no cover
-    from pydantic import BaseConfig  # type: ignore[assignment]
+import pydantic.v1 as pydantic
+import pytest
+from aiohttp import ClientSession
+
+from aiopygismeteo import Gismeteo
+
+
+@pytest.fixture(scope="session")
+def event_loop() -> Iterator[asyncio.AbstractEventLoop]:
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
+async def http_session() -> AsyncIterator[ClientSession]:
+    async with ClientSession() as s:
+        yield s
+
+
+@pytest.fixture()
+def location_id() -> int:
+    # Moscow
+    return 4368
+
+
+@pytest.fixture()
+def coordinates() -> Tuple[float, float]:
+    # Moscow
+    return 55.7522200, 37.6155600
+
+
+@pytest.fixture()
+def search_query() -> str:
+    return "mosc"
+
+
+@pytest.fixture()
+def ipv4_address() -> str:
+    return "8.8.8.8"
+
+
+@pytest.fixture()
+def gismeteo(http_session: ClientSession) -> Gismeteo:
+    return Gismeteo(session=http_session)
+
+
+@pytest.fixture()
+def _pydantic_ignore_extra(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(pydantic.BaseConfig, "extra", pydantic.Extra.ignore)
 
 
 @pytest.fixture(autouse=True)
 def _pydantic_strict(monkeypatch: pytest.MonkeyPatch) -> None:
-    monkeypatch.setattr(BaseConfig, "validate_all", True)
-    monkeypatch.setattr(BaseConfig, "validate_assignment", True)
+    monkeypatch.setattr(pydantic.BaseConfig, "extra", pydantic.Extra.forbid)
+    monkeypatch.setattr(pydantic.BaseConfig, "validate_all", True)
+    monkeypatch.setattr(pydantic.BaseConfig, "validate_assignment", True)
--- tests/test_aiopygismeteo.py
+++ tests/test_aiopygismeteo.py
@@ -1,38 +1,161 @@
 from __future__ import annotations
 
+from ipaddress import IPv4Address
+from typing import Tuple, Type, Union
+
+import pydantic.v1 as pydantic
+import pytest
 from aiohttp import ClientSession
 
-from aiopygismeteo import Gismeteo
+from aiopygismeteo import Gismeteo, models
 
 
-async def test_aiopygismeteo() -> None:
-    async with ClientSession() as session:
-        gm = Gismeteo(session=session)
-        assert gm.session is session
-        await gm.current.by_id(4368)
-        await gm.current.by_coordinates(54.35, 52.52)
+async def test_current_by_id(location_id: int) -> None:
+    gismeteo = Gismeteo(lang="en")
+    r = await gismeteo.current.by_id(location_id)
+    assert isinstance(r, models.current.Model)
 
-        await gm.step3.by_id(4368, days=10)
-        await gm.step3.by_coordinates(54.35, 52.52, days=10)
 
-        await gm.step6.by_id(4368, days=10)
-        await gm.step6.by_coordinates(54.35, 52.52, days=10)
+async def test_current_by_coordinates(
+    gismeteo: Gismeteo, coordinates: Tuple[float, float]
+) -> None:
+    r = await gismeteo.current.by_coordinates(*coordinates)
+    assert isinstance(r, models.current.Model)
 
-        await gm.step24.by_id(4368, days=10)
-        await gm.step24.by_coordinates(54.35, 52.52, days=10)
 
-        await gm.search.by_query("lond")
-        await gm.search.by_coordinates(54.35, 52.52, limit=36)
+@pytest.mark.parametrize("as_list", [True, False])
+async def test_step3_by_id(
+    gismeteo: Gismeteo, location_id: int, as_list: bool
+) -> None:
+    r = await gismeteo.step3.by_id(location_id, days=10, as_list=as_list)
+    if as_list:
+        assert isinstance(r, list)
+    else:
+        assert isinstance(r, models.step3.Model)
+        assert isinstance(r.__root__, list)
 
-    assert gm.lang is None
-    gm.lang = "en"
-    assert gm.lang == "en"
 
-    gm.session = None
-    assert gm.session is None
+@pytest.mark.parametrize("as_list", [True, False])
+async def test_step3_by_coordinates(
+    gismeteo: Gismeteo, coordinates: Tuple[float, float], as_list: bool
+) -> None:
+    r = await gismeteo.step3.by_coordinates(
+        *coordinates, days=10, as_list=as_list
+    )
+    if as_list:
+        assert isinstance(r, list)
+    else:
+        assert isinstance(r, models.step3.Model)
+        assert isinstance(r.__root__, list)
 
-    await gm.search.by_ip("8.8.8.8")
 
-    assert gm.token is None
-    gm.token = ""
-    assert gm.token == ""
+@pytest.mark.parametrize("as_list", [True, False])
+async def test_step6_by_id(
+    gismeteo: Gismeteo, location_id: int, as_list: bool
+) -> None:
+    r = await gismeteo.step6.by_id(location_id, days=10, as_list=as_list)
+    if as_list:
+        assert isinstance(r, list)
+    else:
+        assert isinstance(r, models.step6.Model)
+        assert isinstance(r.__root__, list)
+
+
+@pytest.mark.parametrize("as_list", [True, False])
+async def test_step6_by_coordinates(
+    gismeteo: Gismeteo, coordinates: Tuple[float, float], as_list: bool
+) -> None:
+    r = await gismeteo.step6.by_coordinates(
+        *coordinates, days=10, as_list=as_list
+    )
+    if as_list:
+        assert isinstance(r, list)
+    else:
+        assert isinstance(r, models.step6.Model)
+        assert isinstance(r.__root__, list)
+
+
+@pytest.mark.parametrize("as_list", [True, False])
+async def test_step24_by_id(
+    gismeteo: Gismeteo, location_id: int, as_list: bool
+) -> None:
+    r = await gismeteo.step24.by_id(location_id, days=10, as_list=as_list)
+    if as_list:
+        assert isinstance(r, list)
+    else:
+        assert isinstance(r, models.step24.Model)
+        assert isinstance(r.__root__, list)
+
+
+@pytest.mark.parametrize("as_list", [True, False])
+async def test_step24_by_coordinates(
+    gismeteo: Gismeteo, coordinates: Tuple[float, float], as_list: bool
+) -> None:
+    r = await gismeteo.step24.by_coordinates(
+        *coordinates, days=10, as_list=as_list
+    )
+    if as_list:
+        assert isinstance(r, list)
+    else:
+        assert isinstance(r, models.step24.Model)
+        assert isinstance(r.__root__, list)
+
+
+@pytest.mark.usefixtures("_pydantic_ignore_extra")
+@pytest.mark.parametrize("as_list", [True, False])
+async def test_search_by_query(
+    gismeteo: Gismeteo, search_query: str, as_list: bool
+) -> None:
+    r = await gismeteo.search.by_query(search_query, as_list=as_list)
+    if as_list:
+        assert isinstance(r, list)
+    else:
+        assert isinstance(r, models.search_by_query.Model)
+        assert isinstance(r.__root__, list)
+
+
+@pytest.mark.usefixtures("_pydantic_ignore_extra")
+@pytest.mark.parametrize("as_list", [True, False])
+async def test_search_by_coordinates(
+    gismeteo: Gismeteo, coordinates: Tuple[float, float], as_list: bool
+) -> None:
+    r = await gismeteo.search.by_coordinates(
+        *coordinates, limit=36, as_list=as_list
+    )
+    if as_list:
+        assert isinstance(r, list)
+    else:
+        assert isinstance(r, models.search_by_coordinates.Model)
+        assert isinstance(r.__root__, list)
+
+
+@pytest.mark.usefixtures("_pydantic_ignore_extra")
+@pytest.mark.parametrize("type_", [str, IPv4Address])
+async def test_search_by_ip(
+    gismeteo: Gismeteo, ipv4_address: str, type_: Type[Union[str, IPv4Address]]
+) -> None:
+    r = await gismeteo.search.by_ip(type_(ipv4_address))
+    assert isinstance(r, models.search_by_ip.Model)
+
+
+def test_session(gismeteo: Gismeteo, http_session: ClientSession) -> None:
+    assert gismeteo.session is http_session
+    gismeteo.session = None
+    assert gismeteo.session is None
+
+
+def test_valid_lang(gismeteo: Gismeteo) -> None:
+    assert gismeteo.lang is None
+    gismeteo.lang = "en"
+    assert gismeteo.lang == "en"
+
+
+def test_invalid_lang(gismeteo: Gismeteo) -> None:
+    with pytest.raises(pydantic.ValidationError):
+        gismeteo.lang = "asdf"  # type: ignore[assignment]
+
+
+def test_token(gismeteo: Gismeteo) -> None:
+    assert gismeteo.token is None
+    gismeteo.token = ""
+    assert gismeteo.token == ""
