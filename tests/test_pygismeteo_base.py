5cb2dd8591dc454e8db0d32b28b075b71dcf10ca
monosans <hsyqixco@protonmail.com>
2023-11-04 12:23:27 +0300

    Fix

--- tests/test_pygismeteo_base.py
+++ tests/test_pygismeteo_base.py
@@ -2,4 +2,7 @@ from __future__ import annotations
 
 
 def test_typing_compat() -> None:
-    from pygismeteo_base.typing_compat import Literal, TypeAlias  # noqa: F401
+    from pygismeteo_base.typing_compat import (  # noqa: F401,PLC0415
+        Literal,
+        TypeAlias,
+    )
