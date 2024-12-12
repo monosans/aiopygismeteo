a0e1dfd7e2ff676cff7f7b6423b38fb522152170
monosans <hsyqixco@protonmail.com>
2024-12-12 12:21:23 +0300

    v7.0.1

--- aiopygismeteo/__init__.py
+++ aiopygismeteo/__init__.py
@@ -45,5 +45,5 @@ from pygismeteo_base import models
 from aiopygismeteo import types
 from aiopygismeteo._gismeteo import Gismeteo
 
-__version__ = "7.0.0"
+__version__ = "7.0.1"
 __all__ = ("Gismeteo", "models", "types")
