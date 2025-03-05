b7edb5c9f619fbb6041d17e41a69b3d73fdfc1ad
monosans <hsyqixco@protonmail.com>
2025-03-05 15:19:27 +0300

    perf: don't decode http response

--- src/aiopygismeteo/_http.py
+++ src/aiopygismeteo/_http.py
@@ -10,7 +10,7 @@ class AiohttpClient(BaseHttpClient[ClientSession]):
 
     async def get_response(
         self, endpoint: str, /, *, params: types.Params
-    ) -> str:
+    ) -> bytes:
         params, headers = self._get_params_and_headers(params)
         if self.session is None:
             self.session = ClientSession(
@@ -22,5 +22,4 @@ class AiohttpClient(BaseHttpClient[ClientSession]):
             headers=headers,
             raise_for_status=True,
         ) as response:
-            await response.read()
-        return await response.text()
+            return await response.read()
